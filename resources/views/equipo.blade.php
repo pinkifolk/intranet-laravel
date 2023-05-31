8acf21d8075ac72509b5ddc5775968237be287a4
pinkifolk <ssoliswebmaster@gmail.com>
2023-05-31 17:30:14 -0400

    ready desing views procedimientos beneficios normativas valores

--- resources/views/equipo.blade.php
+++ resources/views/equipo.blade.php
@@ -1,11 +1,35 @@
 @extends('layout.layout')
 @section('content')
-<div class="container mt-12 bg-zinc-500">
-    <h1>equipo</h1>
-    @foreach ($team_get as $item)
-    <li>{{$item->name}}</li>
-    <li>{{$item->last_name}}</li>
-    <li>{{$item->job_title}}</li>
-    @endforeach
+<h1>equipo</h1>
+<div class="grid grid-cols-12 gap-4">
+    <div class="col-start-6 bg-slate-400 text-center">
+        <div class="flex flex-col">
+            <div>Nombre</div>
+            <div>Cargo</div>
+        </div>
+    </div>
+    <div class="bg-slate-500 text-center">
+        <div class="flex flex-col">
+            <div>Nombre</div>
+            <div>Cargo</div>
+        </div>
+    </div>
+    <div class="col-start-1 col-end-13 bg-slate-500">
+        <div class="grid grid-cols-7">
+            <div>A</div>
+            <div>A</div>
+            <div>A</div>
+            <div>A</div>
+            <div>A</div>
+            <div>A</div>
+            <div>A</div>
+        </div>
+    </div>
 </div>
+{{-- @foreach ($team_get as $item)
+<li>{{$item->name}}</li>
+<li>{{$item->last_name}}</li>
+<li>{{$item->job_title}}</li>
+@endforeach --}}
+
 @endsection
--- resources/views/nuestros-valores.blade.php
+++ resources/views/nuestros-valores.blade.php
@@ -1,11 +1,18 @@
 @extends('layout.layout')
 @section('content')
-<div class="container mt-12 bg-zinc-500">
-    <h1>Nuestros Valores</h1>
-    @foreach ($values_get as $item)
-    <li>{{$item->title}}</li>
-    <li>{{$item->detail}}</li>
-    <li>{{$item->url_pdf}}</li>
-    @endforeach
+<h1>Nuestros Valores</h1>
+@foreach ($values_get as $item)
+<div class="grid grid-cols-3 mb-6">
+    <div class="col-span-3">
+        <h5 class="font-bold text-xl">{{$item->title}}</h5>
+    </div>
+    <div class="col-span-2">{{$item->detail}}</div>
+    <div class="flex items-center justify-center">
+        <a href="{{$item->url_pdf}}" target="_bank" title="descargar">
+            <i class="fa-solid fa-file-pdf fa-2xl"></i>
+        </a>
+    </div>
 </div>
+@endforeach
+
 @endsection
