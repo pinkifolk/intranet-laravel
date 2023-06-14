c54a62e75b6425c5a08faa62a4c5accf5f58a3d8
pinkifolk <ssoliswebmaster@gmail.com>
2023-06-14 16:24:52 -0400

    add style to equipo nav

--- resources/views/procedimientos.blade.php
+++ resources/views/procedimientos.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.layout')
 @section('content')
-<h1>Procedimientos</h1>
+<h1 class="text-3xl font-bold mb-5">Procedimientos</h1>
 @foreach ($procedure_get as $item)
-<div class="grid grid-cols-3 mb-6">
+<div class="grid grid-cols-3 mb-6 p-5 hover:bg-slate-300 rounded-md">
     <div class="col-span-3">
         <h5 class="font-bold text-xl">{{$item->title}}</h5>
     </div>
