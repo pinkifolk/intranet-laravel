bab67e0cc98d44df8934274c5867d8cca7637210
pinkifolk <ssoliswebmaster@gmail.com>
2023-07-25 15:12:47 -0400

    add cdn to views

--- resources/views/equipo2.blade.php
+++ resources/views/equipo2.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite('resources/css/app.scss')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     @livewireStyles
 </head>
 
